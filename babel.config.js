0dd6d2eee6776d4a49254a60d793f900517dd929
Bruno Adamo <brunoaraujoadamo@gmail.com>
2019-10-08 20:22:51 -0300

    feature: new component and add a babel plugin that recognize the state structure

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,6 @@ module.exports = {
   presets: [
     "@babel/preset-env", //java scrip - import, export, arrow functions ...
     "@babel/preset-react" //transform react to browser language
-  ]
+  ],
+  plugins: ["@babel/plugin-proposal-class-properties"] //for babel reconize the state structure
 };
--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
 
-import profile from "./assets/profile.jpg";
+import TechList from "./components/TechList";
 
 function App() {
-  return <img width="200" src={profile}></img>;
+  return <TechList></TechList>;
 }
 
 export default App;
